f3d400c26ad01c370e7f426921160b06c492babf
ParamonovIvan <131868856+ParamonovIvan@users.noreply.github.com>
2023-12-02 15:23:21 +0400

    Add files via upload

---
+++ src/components/SearchInput.js
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+
+import css from "../styles/SearchInput.css"
+
+const SearchInput = ({ city, setCity, onKeyPress }) => {
+  return (  
+    <input 
+      type="text" 
+      placeholder="Enter your location"
+      onChange={(e) => setCity(e.target.value)}
+      value={city}
+      onKeyPress={onKeyPress}
+    />      
+  );
+};
+
+export default SearchInput;
